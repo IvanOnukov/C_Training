63c4bea5f8ae6ab4782ce859f4f70502d4a8e4a1
maikal <ivanonukov@ya.ru>
2022-08-29 20:01:34 +0300

    add ignore file

--- template/alg/binary_tree/main.c
+++ template/alg/binary_tree/main.c
@@ -11,13 +11,13 @@ int main() {
     setElemTree(tree, decimal(70), string("D"));
     setElemTree(tree, decimal(55), string("E"));
     printTreeAsList(tree);
-    delElemTree(tree, decimal(60));
+    delElemTree(tree, decimal(50));
     printTreeAsList(tree);
 
-    storeValueTree dfd = getElemTree(tree, decimal(55));
-    printf("%s\n", dfd.string);
-
-    printf("%d\n", elemInTree(tree->node, decimal(70)));
+//    storeValueTree dfd = getElemTree(tree, decimal(55));
+//    printf("%s\n", dfd.string);
+//
+//    printf("%d\n", elemInTree(tree->node, decimal(70)));
 
     freeTree(tree);
     return 0;
